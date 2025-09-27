30c9d6aeae5cd47d29cc4e4c278c5fa5ac78dcef
Artur Serkeli <serkeli2006@gmail.com>
2025-09-27 11:53:09 +0300

    first commit

---
+++ src/main/java/com/artur/java/spacecatsmarket/SpaceCatsMarketApplication.java
@@ -0,0 +1,13 @@
+package com.artur.java.spacecatsmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceCatsMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceCatsMarketApplication.class, args);
+    }
+
+}
